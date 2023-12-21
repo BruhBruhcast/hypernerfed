dd3864b6a7423e9bdb391c0f1965b958cc883dd0
BruhBruhcast <113485007+BruhBruhcast@users.noreply.github.com>
2023-12-21 18:23:19 +1100

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,7 +1,15 @@
 function Dec(x) {
-  return (new Decimal(x))
+    return (new Decimal(x))
 }
 
 function hasUpgrade(x) {
-  return (player.upgrades[x].bought)
+    return (player.upgrades[x].bought)
+}
+
+function buyUpgrade(x) {
+    let res = player.upgrades[x].resource
+    let cost = player.upgrades[x].cost
+    if (res.gte(cost)) {
+        player.upgrades[x].bought = true
+    }
 }
