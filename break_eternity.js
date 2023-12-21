19a18518c29894a040815a887a4718585a100935
BruhBruhcast <113485007+BruhBruhcast@users.noreply.github.com>
2023-12-21 18:20:54 +1100

    added credits

--- break_eternity.js
+++ break_eternity.js
@@ -1,5 +1,4 @@
 // By Patashu
-
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
