624ab86d827e59f766f33a4ffdb659b7c2f9a943
Boi Zak <bruhbruhcast@gmail.com>
2024-02-09 20:52:21 +1100

    bugfix fail

--- main.js
+++ main.js
@@ -24,15 +24,4 @@ function getPointGen() {
     return gain
 }
 
-var updateNumbers = window.setInterval(function() {
-    player.basePoints = player.basePoints.add(getPointGen())
-    player.points = player.basePoints.slog(player.gainSlog).log(player.gainLog).max(0)
-    if (player.basePoints.lte(1)) {
-        player.points = Dec(0)
-    }
-}, 33)
-
-var updateText = window.setInterval(function() {
-    document.getElementById("ptamt").innerHTML = player.basePoints + " base points, which become " + player.points + " points"
-}, 33)
 
